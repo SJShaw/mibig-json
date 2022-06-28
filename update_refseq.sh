243613b1f3d03fca1159bb0706306e31b3d4fd77
Simon Shaw <SJShaw@users.noreply.github.com>
2022-06-28 17:50:46 +0200

    script changes

--- update_refseq.sh
+++ update_refseq.sh
@@ -8,6 +8,7 @@ for bgc in `git grep '"accession"' | grep BGC | grep _ | grep -v retired | cut -
             continue
         fi 
     fi
+    grep $bgc empty_refseq_dates || grep $bgc nonempty_refseq_dates || echo "missing empty state for $bgc"
     test -z "`grep -e 'Gene details extracted from RefSeq' -e 'equivalent GenBank accession' $bgc.json`"
     if [ "$?" == "0" ] ; then
         MESG="add RefSeq gene details and change to GenBank accession"
